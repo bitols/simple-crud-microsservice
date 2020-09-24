88fb6bb70161f27ba6f73aa88050dd240b95b8cb
Raphael Silveira <bitols@gmail.com>
2020-09-24 20:49:49 -0300

    desenvolvimento do projeto

---
+++ simple-crud-microsservice/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simple_crud_microsservice.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public int categoryId { get; set; }
+    }
+}
